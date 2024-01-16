a75a0d03b3df3b961b49b03ae37085511769a59e
Richard Buendia Cordova <buendiacordovarichard@gmail.com>
2024-01-15 22:35:59 -0500

    nav

--- rotoplast/index.php
+++ rotoplast/index.php
@@ -9,9 +9,27 @@
 </head>
 
 <body>
+    <header>
+        <a href="" class="logo"><img src="assets/diecam-logotipo-175w.webp" alt="logo"></a>
+        <ul class="navlist">
+            <li><a href="">INICIO</a></li>
+            <li><a href="">LÍNEA HOGAR</a></li>
+            <li><a href="">AGROINDUSTRIA</a></li>
+            <li><a href="">TIENDA</a></li>
+            <li><a href="">CONTAÁCTENOS</a></li>
+        </ul>
+        <div class="top-icon">
+            <a href=""><i class='bx bx-search'></i></a>
+            <a href=""><i class='bx bx-cart'></i></a>
+            <a href=""><i class='bx bx-user'></i></a>
+        </div>
+    </header>
+    <main>
 
+    </main>
+    <footer>
 
-    <script src="script.js"></script>
+    </footer>
 </body>
 
 </html>
