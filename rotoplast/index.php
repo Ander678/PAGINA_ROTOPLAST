78691185dc1840c548774930bdc01201fa37ec82
Richard Buendia Cordova <buendiacordovarichard@gmail.com>
2024-01-15 23:42:31 -0500

    nav 2

--- rotoplast/index.php
+++ rotoplast/index.php
@@ -5,7 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DIECAM</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600;700&family=Chivo:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Raleway:wght@100;200;400;500;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap');
+    </style>
 </head>
 
 <body>
@@ -16,16 +20,32 @@
             <li><a href="">LÍNEA HOGAR</a></li>
             <li><a href="">AGROINDUSTRIA</a></li>
             <li><a href="">TIENDA</a></li>
-            <li><a href="">CONTAÁCTENOS</a></li>
+            <li><a href="">CONTÁCTENOS</a></li>
         </ul>
         <div class="top-icon">
             <a href=""><i class='bx bx-search'></i></a>
             <a href=""><i class='bx bx-cart'></i></a>
             <a href=""><i class='bx bx-user'></i></a>
+            <div class="bx bx-menu" id="menu-icon"></div>
         </div>
     </header>
     <main>
+        <section class="inicio">
+            <div class="inicio-text">
+                <h1>DIECAM INVERSIONES S.A.C</h1>
+                <h4>Distribuidor autorizado y especializado de Rotoplas.
 
+                    Comercialización de todos sus productos a nivel nacional
+
+                    ¡Conoce los beneficios y promociones que tenemos para ti!</h4>
+                <div class="inicio-btn">
+                    <a href="#">COMPRAR</a>
+                </div>
+            </div>
+            <div class="inicio-img">
+                <img src="" alt="">
+            </div>
+        </section>
     </main>
     <footer>
 
