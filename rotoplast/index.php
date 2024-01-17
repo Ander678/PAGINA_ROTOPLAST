0fe07f1b3e1d9984b3da293135254f504ff8897d
Richard Buendia Cordova <buendiacordovarichard@gmail.com>
2024-01-16 23:04:52 -0500

    de cero ctmr

--- rotoplast/index.php
+++ rotoplast/index.php
@@ -14,38 +14,10 @@
 
 <body>
     <header>
-        <a href="" class="logo"><img src="assets/diecam-logotipo-175w.webp" alt="logo"></a>
-        <ul class="navlist">
-            <li><a href="">INICIO</a></li>
-            <li><a href="">LÍNEA HOGAR</a></li>
-            <li><a href="">AGROINDUSTRIA</a></li>
-            <li><a href="">TIENDA</a></li>
-            <li><a href="">CONTÁCTENOS</a></li>
-        </ul>
-        <div class="top-icon">
-            <a href=""><i class='bx bx-search'></i></a>
-            <a href=""><i class='bx bx-cart'></i></a>
-            <a href=""><i class='bx bx-user'></i></a>
-            <div class="bx bx-menu" id="menu-icon"></div>
-        </div>
+
     </header>
     <main>
-        <section class="inicio">
-            <div class="inicio-text">
-                <h1>DIECAM INVERSIONES S.A.C</h1>
-                <h4>Distribuidor autorizado y especializado de Rotoplas.
 
-                    Comercialización de todos sus productos a nivel nacional
-
-                    ¡Conoce los beneficios y promociones que tenemos para ti!</h4>
-                <div class="inicio-btn">
-                    <a href="#">COMPRAR</a>
-                </div>
-            </div>
-            <div class="inicio-img">
-                <img src="assets/img1.png" alt="">
-            </div>
-        </section>
     </main>
     <footer>
 
