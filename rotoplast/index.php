5788f0ac2567c9833effb4e04c9f182536a0878d
Richard Buendia Cordova <buendiacordovarichard@gmail.com>
2024-01-17 23:26:25 -0500

    de cero ctmr

--- rotoplast/index.php
+++ rotoplast/index.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -8,16 +12,26 @@
     <link rel="stylesheet" href="css/style.css" />
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600;700&family=Chivo:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Raleway:wght@100;200;400;500;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
     </style>
 </head>
 
 <body>
     <header>
-
+        <a href="" class="logo"><img src="img/diecam-logotipo-175w.webp" alt=""></a>
+        <div class="bx bx-menu" id="menu-icon"></div>
+        <ul class="navbar">
+            <li><a href="#inicio">INICIO</a></li>
+            <li><a href="#lineaHogar">LINEA HOGAR</a></li>
+            <li><a href="#agroindustria">AGROINDUSTRIA</a></li>
+            <li><a href="#tienda">TIENDA</a></li>
+            <li><a href="#contactenos">CONTÁCTENOS</a></li>
+        </ul>
     </header>
     <main>
+        <section class="container hero">
 
+        </section>
     </main>
     <footer>
 
