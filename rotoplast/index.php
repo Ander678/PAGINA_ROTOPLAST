7bb196f5ac60f2f44948b3c4221fa9995ae98fe3
Richard Buendia Cordova <buendiacordovarichard@gmail.com>
2024-01-18 00:46:33 -0500

    tengo sueño

--- rotoplast/index.php
+++ rotoplast/index.php
@@ -14,14 +14,15 @@
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
     </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 </head>
 
 <body>
     <header>
-        <a href="" class="logo"><img src="img/diecam-logotipo-175w.webp" alt=""></a>
+        <a href="" class="logo"><img src="img/diecam-logotipo-175w.webp" alt="logo"></a>
         <div class="bx bx-menu" id="menu-icon"></div>
         <ul class="navbar">
-            <li><a href="#inicio">INICIO</a></li>
+            <li><a href="#inicio" class="home-active">INICIO</a></li>
             <li><a href="#lineaHogar">LINEA HOGAR</a></li>
             <li><a href="#agroindustria">AGROINDUSTRIA</a></li>
             <li><a href="#tienda">TIENDA</a></li>
@@ -29,13 +30,58 @@
         </ul>
     </header>
     <main>
-        <section class="container hero">
+        <section class="home swiper" id="home">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide container">
+                    <img src="img/fondo2.jpg" alt="">
+                    <div class="home-text">
+                        <span>COMIENZA YA!</span>
+                        <h1>DIECAM INVERSIONES S.A.C</h1>
+                        <h2>Distribuidor autorizado y especializado de Rotoplas.
 
+                            Comercialización de todos sus productos a nivel nacional
+
+                            ¡Conoce los beneficios y promociones que tenemos para ti!</h2>
+                        <a href="" class="btnHome">COMPRAR</a>
+                    </div>
+                </div>
+                <div class="swiper-slide container">
+                    <img src="img/fondo2.jpg" alt="">
+                    <div class="home-text">
+                        <span>COMIENZA YA!</span>
+                        <h3>DIECAM INVERSIONES S.A.C</h3>
+                        <h2>Distribuidor autorizado y especializado de Rotoplas.
+
+                            Comercialización de todos sus productos a nivel nacional
+
+                            ¡Conoce los beneficios y promociones que tenemos para ti!</h2>
+                        <a href="" class="btnHome">COMPRAR</a>
+                    </div>
+                </div>
+                <div class="swiper-slide container">
+                    <img src="img/fondo2.jpg" alt="">
+                    <div class="home-text">
+                        <span>COMIENZA YA!</span>
+                        <h3>DIECAM INVERSIONES S.A.C</h3>
+                        <h2>Distribuidor autorizado y especializado de Rotoplas.
+
+                            Comercialización de todos sus productos a nivel nacional
+
+                            ¡Conoce los beneficios y promociones que tenemos para ti!</h2>
+                        <a href="" class="btnHome">COMPRAR</a>
+                    </div>
+                </div>
+            </div>
+            <!--<div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>-->
+            <div class="swiper-pagination"></div>
         </section>
     </main>
     <footer>
 
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="js/swip.js"></script>
 </body>
 
 </html>
