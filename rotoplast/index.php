576a82f7d8f4a16a8e485429122c1a426568fb65
Richard Buendia Cordova <buendiacordovarichard@gmail.com>
2024-01-19 17:12:03 -0500

    huevadas

--- rotoplast/index.php
+++ rotoplast/index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DIECAM</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
     </style>
 </head>
+
 <body>
     <header>
         <a href="" class="logo"><img src="img/Nuevo_Lienzo_-_2.png" alt=""></a>
@@ -47,7 +49,7 @@
                             <option value="filtros">Filtros, cartuchos y pruificadores domésticos</option>
                             <option value="bombas">Bombas para tanques</option>
                             <option value="termas">Termas</option>
-                        </select>  
+                        </select>
                     </div>
                     <input type="submit" name="" id="" class="btnBuscar" value="Buscar">
                 </form>
@@ -105,7 +107,7 @@
                     <p>Elige el servicio de envío que mejor se adapte a tus necesidades. Realizamos envíos express dentro de Lima, Perú.</p>
                 </div>
                 <div class="box">
-                    <i class='bx bxs-timer' ></i>
+                    <i class='bx bxs-timer'></i>
                     <h3>Entrega inmediata</h3>
                     <p>Compra online nuestros productos y te garantizamos que lleguen rápidamente.</p>
                 </div>
@@ -114,31 +116,32 @@
     </main>
     <footer class="pie-pagina">
         <div class="group-1">
-             <div class="box">
-                 <figure>
-                     <a href="#">
-                         <img src="img/Logo.png" alt="Logo">
-                     </a>
-                 </figure>
-             </div>
-             <div class="box">
-                 <h2>SOBRE NOSOTROS</h2>
-                 <p>asadawda</p>
-                 <p>adasdw</p>
-             </div>
-             <div class="box">
-                 <h2>SIGUENOS</h2>
-                 <div class="red-social">
+            <div class="box">
+                <figure>
+                    <a href="#">
+                        <img src="img/Logo.png" alt="Logo">
+                    </a>
+                </figure>
+            </div>
+            <div class="box">
+                <h2>SOBRE NOSOTROS</h2>
+                <p>asadawda</p>
+                <p>adasdw</p>
+            </div>
+            <div class="box">
+                <h2>SIGUENOS</h2>
+                <div class="red-social">
                     <i class='bx bxl-facebook'></i>
-                    <i class='bx bxl-instagram' ></i>
-                    <i class='bx bxl-whatsapp' ></i>
-                 </div>
-             </div>
+                    <i class='bx bxl-instagram'></i>
+                    <i class='bx bxl-whatsapp'></i>
+                </div>
+            </div>
         </div>
         <div class="group-2">
-         <small>&copy; 2024 <b>DIECAM</b> - Todos los derechos reservados</small>
+            <small>&copy; 2024 <b>DIECAM</b> - Todos los derechos reservados</small>
         </div>
-         </footer>
+    </footer>
     <script src="main.js"></script>
 </body>
+
 </html>
