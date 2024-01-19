c4d188a3b7c9c963e7d98801e550ab687e1bbffa
Richard Buendia Cordova <buendiacordovarichard@gmail.com>
2024-01-19 17:04:11 -0500

    spawn 2

--- rotoplast/index.php
+++ rotoplast/index.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -9,109 +5,143 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DIECAM</title>
-    <link rel="icon" href="img/diecam-logotipo-175w.webp" type="image/x-icon">
-    <link rel="stylesheet" href="css/style.css" />
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
     </style>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 </head>
 
 <body>
     <header>
-        <a href="" class="logo"><img src="img/diecam-logotipo-175w.webp" alt="logo"></a>
+        <a href="" class="logo"><img src="img/Nuevo_Lienzo_-_2.png" alt=""></a>
         <div class="bx bx-menu" id="menu-icon"></div>
         <ul class="navbar">
-            <li><a href="#inicio" class="home-active">INICIO</a></li>
-            <li><a href="#lineaHogar">LINEA HOGAR</a></li>
+            <li><a href="#incio">INCIO</a></li>
+            <li><a href="#linea-hogar">LÍNEA HOGAR</a></li>
             <li><a href="#agroindustria">AGROINDUSTRIA</a></li>
             <li><a href="#tienda">TIENDA</a></li>
             <li><a href="#contactenos">CONTÁCTENOS</a></li>
         </ul>
+        <div class="header-btn">
+            <a href="" class="registrarse">Registrarse</a>
+            <a href="" class="ingresar">Ingresar</a>
+        </div>
     </header>
     <main>
-        <section class="home swiper" id="home">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide container">
-                    <img src="img/fondo2.jpg" alt="">
-                    <div class="home-text">
-                        <span>COMIENZA YA!</span>
-                        <h1>DIECAM INVERSIONES S.A.C</h1>
-                        <h2>Distribuidor autorizado y especializado de Rotoplas.
-
-                            Comercialización de todos sus productos a nivel nacional
-
-                            ¡Conoce los beneficios y promociones que tenemos para ti!</h2>
-                        <a href="" class="btnHome">COMPRAR</a>
-                    </div>
-                </div>
-                <div class="swiper-slide container">
-                    <img src="img/fondo2.jpg" alt="">
-                    <div class="home-text">
-                        <span>COMIENZA YA!</span>
-                        <h3>DIECAM INVERSIONES S.A.C</h3>
-                        <h2>Distribuidor autorizado y especializado de Rotoplas.
-
-                            Comercialización de todos sus productos a nivel nacional
-
-                            ¡Conoce los beneficios y promociones que tenemos para ti!</h2>
-                        <a href="" class="btnHome">COMPRAR</a>
-                    </div>
-                </div>
-                <div class="swiper-slide container">
-                    <img src="img/fondo2.jpg" alt="">
-                    <div class="home-text">
-                        <span>COMIENZA YA!</span>
-                        <h3>DIECAM INVERSIONES S.A.C</h3>
-                        <h2>Distribuidor autorizado y especializado de Rotoplas.
-
-                            Comercialización de todos sus productos a nivel nacional
-
-                            ¡Conoce los beneficios y promociones que tenemos para ti!</h2>
-                        <a href="" class="btnHome">COMPRAR</a>
-                    </div>
+        <section class="home" id="home">
+            <div class="text">
+                <h1><span>DIECAM</span> INVERSIONES S.A.C</h1>
+                <p>Distribuidor autorizado y especializado de Rotoplas. Comercialización de todos sus productos a nivel nacional</p>
+                <div class="btn-home">
+                    <a href="">COMPRAR</a>
                 </div>
             </div>
-            <!--<div class="swiper-button-next"></div>
-            <div class="swiper-button-prev"></div>-->
-            <div class="swiper-pagination"></div>
-        </section>
-        <section>
-            <div>
 
+            <div class="form-container">
+                <form action="">
+                    <div class="input-box">
+                        <span>Categoría</span>
+                        <select name="select">
+                            <option value="biodigestores">Biodigestores</option>
+                            <option value="cisternas">Cisternas</option>
+                            <option value="tanques" selected>Tanques y Accesorios domésticos</option>
+                            <option value="filtros">Filtros, cartuchos y pruificadores domésticos</option>
+                            <option value="bombas">Bombas para tanques</option>
+                            <option value="termas">Termas</option>
+                        </select>
+                    </div>
+                    <input type="submit" name="" id="" class="btnBuscar" value="Buscar">
+                </form>
+            </div>
+        </section>
+
+        <!--lo mas vendido mi amor-->
+        <section class="populares" id="populares">
+            <div class="heading">
+                <span>Mas populares</span>
+                <h2>Los productos mas vendidos</h2>
+            </div>
+            <div class="populares-container">
+                <div class="box">
+                    <div class="box-img">
+                        <img src="img/Lavado-de-tinaco.png" alt="">
+                    </div>
+                    <h4>Rotoplas 660L</h4>
+                    <a href="" class="btn">Consultar precio YA!</a>
+                </div>
+
+                <div class="box">
+                    <div class="box-img">
+                        <img src="img/Lavado-de-tinaco.png" alt="">
+                    </div>
+                    <h4>Rotoplas 660L</h4>
+                    <a href="" class="btn">Consultar precio YA!</a>
+                </div>
+
+                <div class="box">
+                    <div class="box-img">
+                        <img src="img/Lavado-de-tinaco.png" alt="">
+                    </div>
+                    <h4>Rotoplas 660L</h4>
+                    <a href="" class="btn">Consultar precio YA!</a>
+                </div>
+            </div>
+        </section>
+
+        <!--los beneficios pues bbita-->
+        <section class="beneficios" id="beneficios">
+            <div class="heading">
+                <span>Soluciones para el hogar, campo e industria</span>
+                <h2>Tenemos los mejores servicios</h2>
+            </div>
+            <div class="beneficios-container">
+                <div class="box">
+                    <i class='bx bxs-paper-plane'></i>
+                    <h3>Entrega inmediata</h3>
+                    <p>Compra online nuestros productos y te garantizamos que lleguen rápidamente.</p>
+                </div>
+                <div class="box">
+                    <i class='bx bxs-truck'></i>
+                    <h3>Servicio express dentro de Lima</h3>
+                    <p>Elige el servicio de envío que mejor se adapte a tus necesidades. Realizamos envíos express dentro de Lima, Perú.</p>
+                </div>
+                <div class="box">
+                    <i class='bx bxs-timer'></i>
+                    <h3>Entrega inmediata</h3>
+                    <p>Compra online nuestros productos y te garantizamos que lleguen rápidamente.</p>
+                </div>
             </div>
         </section>
     </main>
     <footer class="pie-pagina">
-   <div class="group-1">
-        <div class="box">
-            <figure>
-                <a href="#">
-                    <img src="img/Logo.png" alt="Logo">
-                </a>
-            </figure>
-        </div>
-        <div class="box">
-            <h2>SOBRE NOSOTROS</h2>
-            <p>asadawda</p>
-            <p>adasdw</p>
-        </div>
-        <div class="box">
-            <h2>SIGUENOS</h2>
-            <div class="red-social">
-            <i class='bx bxl-facebook'></i>
-            <i class='bx bxl-instagram' ></i>
-            <i class='bx bxl-whatsapp' ></i>
+        <div class="group-1">
+            <div class="box">
+                <figure>
+                    <a href="#">
+                        <img src="img/Logo.png" alt="Logo">
+                    </a>
+                </figure>
+            </div>
+            <div class="box">
+                <h2>SOBRE NOSOTROS</h2>
+                <p>asadawda</p>
+                <p>adasdw</p>
+            </div>
+            <div class="box">
+                <h2>SIGUENOS</h2>
+                <div class="red-social">
+                    <i class='bx bxl-facebook'></i>
+                    <i class='bx bxl-instagram'></i>
+                    <i class='bx bxl-whatsapp'></i>
+                </div>
             </div>
         </div>
-   </div>
-   <div class="group-2">
-    <small>&copy; 2024 <b>DIECAM</b> - Todos los derechos reservados</small>
-   </div>
+        <div class="group-2">
+            <small>&copy; 2024 <b>DIECAM</b> - Todos los derechos reservados</small>
+        </div>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="js/swip.js"></script>
+    <script src="main.js"></script>
 </body>
 
 </html>
