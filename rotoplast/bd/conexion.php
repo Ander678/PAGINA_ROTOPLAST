8ef867353b697bec53b8a5c36503c49d7151b8b9
Richard Buendia Cordova <buendiacordovarichard@gmail.com>
2024-02-08 15:59:58 -0500

    nuevo mapa

---
+++ rotoplast/bd/conexion.php
@@ -0,0 +1,6 @@
+<?php
+$connn = new mysqli("localhost", "root", "", "apetito", 3307);
+$connn->set_charset("utf8");
+if ($connn->connect_error) {
+    die("Error de conecion: " . $connn->connect_error);
+}
