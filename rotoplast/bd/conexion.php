bb948532fed688d6c86211de786be6281a11a4c2
John Doe <anderson_vasquezh@outlook.com>
2024-02-10 01:40:30 -0500

    Ahi viene el buffet

--- rotoplast/bd/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-$connn = new mysqli("localhost", "root", "", "apetito", 3307);
-$connn->set_charset("utf8");
-if ($connn->connect_error) {
-    die("Error de conecion: " . $connn->connect_error);
-}
