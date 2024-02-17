f8990e95b23f3690c301ed9da22b230f2206eb0d
Richard Buendia Cordova <buendiacordovarichard@gmail.com>
2024-02-17 00:34:24 -0500

    nuevos colores nuevo slider y cambio mapa

--- rotoplast/js/mapa.js
+++ rotoplast/js/mapa.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([-12.191811689910212, -76.92430877310264], 16);
+var map = L.map('map').setView([-12.191296197093735, -76.92283585671555], 16);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-12.191811689910212, -76.92430877310264]).addTo(map)
-    .bindPopup('ABIERTO 🟢')
+L.marker([-12.191296197093735, -76.92283585671555]).addTo(map)
+    .bindPopup('DIECAM - Av. La Mariscala 828, Lima 15818 🟢')
     .openPopup();
